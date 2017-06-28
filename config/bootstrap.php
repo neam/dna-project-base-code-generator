c800b7bb6a64a225b0c24225e741daada389c7d7
Fredrik Wollsén <fredrik@neam.se>
2017-06-28 12:22:18 +0300

    Removed phundament

--- config/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-Yii::setAlias('@app', dirname(__DIR__).'/..');
-Yii::setAlias('@project', DNA_PROJECT_PATH);
-
-$alias = getenv('CODE_GENERATOR_BOOTSTRAP_INCLUDE_ALIAS');
-if (!empty($alias)) {
-    require(Yii::getAlias($alias));
-}
