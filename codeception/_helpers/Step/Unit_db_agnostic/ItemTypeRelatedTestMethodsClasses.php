b12de81538bb8dc6cbad212a5ac3b7bc9cdc638a
Fredrik Wollsén <fredrik@neam.se>
2017-06-28 14:25:45 +0300

    First actual code generation based on item types

--- codeception/_helpers/Step/Unit_db_agnostic/ItemTypeRelatedTestMethodsClasses.php
+++ codeception/_helpers/Step/Unit_db_agnostic/ItemTypeRelatedTestMethodsClasses.php
@@ -2,6 +2,8 @@
 
 namespace Step\Unit_db_agnostic;
 
+use ItemTypes;
+
 class ItemTypeRelatedTestMethodsClasses extends \DbAgnosticCodeGuy
 {
 
@@ -11,6 +13,40 @@ class ItemTypeRelatedTestMethodsClasses extends \DbAgnosticCodeGuy
     public function iGenerateItemRelatedTestMethodClassesForAllItemTypes()
     {
 
+        // read item types from ItemTypes
+
+        codecept_debug(ItemTypes::all());
+
+        // read item types with metadata from schema.xml
+
+        $modelNamespace = 'propel\\models';
+        $contentModelMetadataJsonPath = \Paths::dna() . DIRECTORY_SEPARATOR . 'content-model-metadata.json';
+
+        // Load content model metadata
+        $contentModelMetadata = new \sq_personal_unit\dna\config\ContentModelMetadata(
+            $contentModelMetadataJsonPath
+        );
+
+        $itemTypes = $contentModelMetadata->getItemTypes();
+
+        //codecept_debug($itemTypes);
+
+        // foreach item type, generate file
+
+        foreach (ItemTypes::all() as $itemTypePhpName => $dbTable) {
+
+            ob_start();
+            include \Paths::dna(
+                ) . DIRECTORY_SEPARATOR . 'generators/dna-project-base-code-generator-templates/test_crud/codeception/item-type-related-test-methods-class.php';
+            $contents = ob_get_clean();
+
+            file_put_contents(
+                \Paths::dna(
+                ) . DIRECTORY_SEPARATOR . 'tests/codeception/_helpers/Step/Unit_db_dependent/crud/' . $itemTypePhpName . 'RelatedTestMethods.php',
+                $contents
+            );
+
+        }
 
     }
 
